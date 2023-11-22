4e171dc1fd8ecdc6922fc16b366235db390654cd
mikk39k1 <mikk39k1@stud.kea.dk>
2023-11-22 14:19:19 +0100

    JWT access_token working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+"use client";
+
 import Image from 'next/image'
 import Login from '@/components/login/Login'
 
 export default function Home() {
+
+const handleClick = async () => {
+  const response = await fetch('http://localhost:8080/api/v1/auth/user', {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  console.log("RESPONSE: ", response.json());
+}
+
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Login />
+      <button onClick={() => handleClick()}>Hello</button>
     </main>
   )
 }
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Link from "next/link";
 
 const Login = () => {
+
+
   return (
     <>
       <div className=' border-2 border-black p-4 mb-10 hover:cursor-pointer'>
+
         <Link href="http://localhost:8080/oauth2/authorize/google">Signin with Google</Link>
       </div>
       <div className=' border-2 border-black p-4 hover:cursor-pointer'>
