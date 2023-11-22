86651a4ce89ac08996062e577086ea72a950ae04
Ingeman366 <oliv366x@stud.kea.dk>
2023-11-22 22:44:54 +0100

    Very simple if-else condition for each week-loop

--- src/components/monthcalendar/CalendarMonthView.tsx
+++ src/components/monthcalendar/CalendarMonthView.tsx
@@ -7,31 +7,35 @@ import endOfMonth from 'date-fns/endOfMonth';
 import startOfMonth from 'date-fns/startOfMonth';
 import getWeeksInMonth from 'date-fns/getWeeksInMonth';
 
-
-
+const daysinWeek: string[] = ['Mandag', 'Tirdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'];
+const monthsInYear: string[] = ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'];
+const currentDate = new Date('December 2, 2029, 02:00:00');
 const CalendarMonthView = () => {
-    const daysinWeek: string[] = ['Mandag', 'Tirdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'];
-    const monthsInYear: string[] = ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'];
-    const currentDate = new Date('December 2, 2031, 02:00:00');
-    //const currentDate: Date = new Date(); // Initialises the current date for the first load
-    const [monthsInYearIndex, setMonthsInYearIndex] = useState(currentDate.getMonth()); // Sets the current month for the StringArray Print.
-    const [firstDayOfMonth, setFirstDayOfMonth] = useState(startOfMonth(currentDate)); // Sets the first date of the month
-    const [firstWeekOfMonth, setFirstWeekOfMonth] = useState(getISOWeek(firstDayOfMonth)); // Sets first weeknumber of the first week in the month
-    const [lastWeekOfMonth, setLastWeekOfMonth] = useState(getISOWeek(endOfMonth(firstDayOfMonth))); // Sets last weeknumber of the last week in the month
-
+    
+    // An object used to set certain values for calculating the calender grid. Updates when the month is changed and renders the page
+    const [dateInfo, setDateInfo] = useState({
+        monthsInYearIndex: currentDate.getMonth(),
+        firstDayOfMonth: startOfMonth(currentDate),
+        firstWeekOfMonth: getISOWeek(startOfMonth(currentDate)),
+        lastWeekOfMonth: getISOWeek(endOfMonth(startOfMonth(currentDate))),
+      });
 
     /** 
     * Function that handles the click on the arrows to change the month
     * @param monthSwapValue is the value that is added to the current month index, to swap the month
     */
     function handleChangeMonthClick(monthSwapValue: number) {
-        const newDate = new Date(firstDayOfMonth);
-
-        newDate.setMonth(newDate.getMonth() + monthSwapValue); // Swap the month to the next or previous month
-        setFirstDayOfMonth(newDate); // Sets the first day of the "new" month
-        setMonthsInYearIndex(newDate.getMonth()); // Sets the month index for the StringArray Print
-        setFirstWeekOfMonth(getISOWeek(newDate)); // Sets the first weeknumber of the "new" month
-        setLastWeekOfMonth((getISOWeek(endOfMonth(newDate)))); //Sets the last weeknumber of the "new" month
+        setDateInfo(prevDateInfo => {
+            const newDate = new Date(prevDateInfo.firstDayOfMonth.getTime());
+            newDate.setMonth(newDate.getMonth() + monthSwapValue);
+    
+            return {
+                monthsInYearIndex: newDate.getMonth(),
+                firstDayOfMonth: newDate,
+                firstWeekOfMonth: getISOWeek(newDate),
+                lastWeekOfMonth: getISOWeek(endOfMonth(newDate)),
+            };
+        });
     }
 
     /**
@@ -49,58 +53,72 @@ const CalendarMonthView = () => {
     * @param firstWeekOfMonth 
     * @param firstDayOfMonth 
     * @param lastWeekOfMonth 
-    * @returns Returns the whole calender grid. It's an array, with arrays at each index, with date components inside
+    * @returns Returns the whole calender grid. 2D JSX.Element array, with days, weeks and date-components inside
     */
-    const generateCalenderGrid = (firstWeekOfMonth: number, firstDayOfMonth: Date, lastWeekOfMonth: number) => {
-        const startDateOfWeek = startOfWeek(firstDayOfMonth, { weekStartsOn: 1 });
+    const generateCalenderGrid = () => {
         const rows = [];
-        startDateOfWeek.setHours(startDateOfWeek.getHours() + 1);
-
-        //const lastDayOfMonth: Date = new Date(firstDayOfMonth.getFullYear(), firstDayOfMonth.getMonth()+1);
-        const lastDayOfMonth: Date = new Date(firstDayOfMonth.getFullYear(), firstDayOfMonth.getMonth() + 1, 0, 23, 59, 59, 999);
-        //const endDateOfWeek: Date = startOfWeek(lastDayOfMonth, { weekStartsOn: 1 });
-        const endWeek: number = getISOWeek(lastDayOfMonth);
+        const startDateOfWeek = startOfWeek(dateInfo.firstDayOfMonth, { weekStartsOn: 1 });
+        startDateOfWeek.setHours(startDateOfWeek.getHours() + 1);       
+        const lastDayOfMonth: Date = new Date(dateInfo.firstDayOfMonth.getFullYear(), dateInfo.firstDayOfMonth.getMonth() + 1, 0, 23, 59, 59, 999);
+    
+        let endWeek: number = getISOWeek(lastDayOfMonth);
         const numbersOfWeeksInMonth = getWeeksInMonth(startDateOfWeek);
         let firstDayOfWeekToMap: Date = startDateOfWeek;
 
         rows.push(generateWeekDays());
-            // January where first week is 52;
-            if (firstWeekOfMonth === 52) {
-                for (let i = 0; i <= lastWeekOfMonth; i++) {
-                    rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap, lastWeekOfMonth))
-                }
-            } 
-            // January where first week is 53;
-            else if (firstWeekOfMonth === 53) {
-                for (let i = 0; i <= lastWeekOfMonth; i++) {
-                    rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap, lastWeekOfMonth))
-                }
+
+        /*
+
+        // January where first week is 52;
+        if (dateInfo.firstWeekOfMonth === 52) {
+            for (let i = 0; i <= dateInfo.lastWeekOfMonth; i++) {
+                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
             }
+        } 
+        // January where first week is 53;
+        else if (dateInfo.firstWeekOfMonth === 53) {
+            for (let i = 0; i <= dateInfo.lastWeekOfMonth; i++) {
+                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
+            }
+        }
         // December where last week is 1;
-        else if (lastWeekOfMonth === 1) {
-            for (let i = firstWeekOfMonth; i <= (firstWeekOfMonth + numbersOfWeeksInMonth); i++) {
+        else if (dateInfo.lastWeekOfMonth === 1) {
+            for (let i = dateInfo.firstWeekOfMonth; i <= (dateInfo.firstWeekOfMonth + numbersOfWeeksInMonth); i++) {
+                /*
+                This was needed earlier. Not confident deleting it yet.
                 if (i == 54) {
                     break;
                 }
-                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap, lastWeekOfMonth))
+                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
             }
         }
         // December where last week is 53;
-        else if (lastWeekOfMonth === 53) {
-            for (let i = firstWeekOfMonth; i <= endWeek; i++) {
-
-            
-                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap, lastWeekOfMonth))
+        else if (dateInfo.lastWeekOfMonth === 53) {
+            for (let i = dateInfo.firstWeekOfMonth; i <= endWeek; i++) {
+                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
             }
         }
 
         // Regular month
         else {
-            for (let i = firstWeekOfMonth; i <= lastWeekOfMonth; i++) {
-
-                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap, lastWeekOfMonth))
+            for (let i = dateInfo.firstWeekOfMonth; i <= dateInfo.lastWeekOfMonth; i++) {
+                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
             }
         }
+        */
+    let startWeek = dateInfo.firstWeekOfMonth;
+    
+
+    if (dateInfo.lastWeekOfMonth === 1) {
+       endWeek = dateInfo.firstWeekOfMonth + numbersOfWeeksInMonth;
+    } else if (dateInfo.lastWeekOfMonth === 53) {
+        endWeek = endWeek;
+    }
+
+    for (let i = startWeek; i <= endWeek; i++) {
+        rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap));
+    }
+
 
         return rows;
     }
@@ -109,28 +127,28 @@ const CalendarMonthView = () => {
     * @param firstWeekOfMonth is required for pushing the weeknumber in the array
     * @param firstDateToMap is required to map the first day in a week
     * @param lastWeekOfMonth is required for push the weeknumber when december ends with week 1
-    * @returns Returns a cell array, 8 columns long. first cell is weekNumber, the rest are mon-sun DayInMonth-components.
+    * @returns Returns 1 row array, 8 columns long. first cell is weekNumber, the rest are mon-sun DayInMonth-components.
     */
-    const generateDatesOfMonth = (firstWeekOfMonth: number, firstDateToMap: Date, lastWeekOfMonth: number) => {
+    const generateDatesOfMonth = (weekToMap: number, dateToMap: Date) => {
         const cells = [];
-        if (firstWeekOfMonth === 0 && getISOWeek(firstDateToMap) === 52) {
+        if (weekToMap === 0 && getISOWeek(dateToMap) === 52) {
             cells.push(<div key="weekNumber" className='font-bold text-center'>{52}</div>);
-        } else if (firstWeekOfMonth === 0 && getISOWeek(firstDateToMap) === 53) {
+        } else if (weekToMap === 0 && getISOWeek(dateToMap) === 53) {
             cells.push(<div key="weekNumber" className='font-bold text-center'>{53}</div>);
-        } else if (firstWeekOfMonth === 53 && getISOWeek(firstDateToMap) === 1) {
+        } else if (weekToMap === 53 && getISOWeek(dateToMap) === 1) {
             cells.push(<div key="weekNumber" className='font-bold text-center'>{1}</div>);
         } else {
-            cells.push(<div key="weekNumber" className='font-bold text-center'>{firstWeekOfMonth}</div>);
+            cells.push(<div key="weekNumber" className='font-bold text-center'>{weekToMap}</div>);
         }
 
         for (let i = 0; i < 7; i++) {
 
-            const dateToMap = new Date(firstDateToMap)
+            const nextDateToMap = new Date(dateToMap)
             cells.push(
                 <div key={i}>
-                    <DayInMonth dateToMap={dateToMap} isDateInMonth={firstDayOfMonth.getMonth() == dateToMap.getMonth()} />
+                    <DayInMonth dateToMap={nextDateToMap} isDateInMonth={dateInfo.firstDayOfMonth.getMonth() == nextDateToMap.getMonth()} />
                 </div>);
-            firstDateToMap.setDate(firstDateToMap.getDate() + 1);
+            dateToMap.setDate(dateToMap.getDate() + 1);
         }
         return cells;
     }
@@ -146,10 +164,10 @@ const CalendarMonthView = () => {
             <div onClick={() => handleChangeMonthClick(1)}
                 className='p-2 absolute'>{"->"}</div>
             <br></br>
-            <div className='p-2 flex-item text-center font-bold'>{monthsInYear[monthsInYearIndex] + ' ' + firstDayOfMonth.getFullYear()}</div>
+            <div className='p-2 flex-item text-center font-bold'>{monthsInYear[dateInfo.monthsInYearIndex] + ' ' + dateInfo.firstDayOfMonth.getFullYear()}</div>
 
             <div className={`grid grid-cols-8 `}>
-                {generateCalenderGrid(firstWeekOfMonth, firstDayOfMonth, lastWeekOfMonth)}
+                {generateCalenderGrid()}
             </div>
 
         </div>
