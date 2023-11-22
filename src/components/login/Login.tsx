40aeecf6e8658a34b7ab44304da69c7bff4dc4e3
mikk39k1 <mikk39k1@stud.kea.dk>
2023-11-22 13:29:21 +0100

    stash

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Login = () => {
   return (
     <div>
-        <Link href="http://localhost:8080/oauth2/authorization/google">Signin with Google</Link>
+        <Link href="http://localhost:8080/oauth2/authorize">Signin with Google</Link>
         </div>
   )
 }
