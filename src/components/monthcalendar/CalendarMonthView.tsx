4f6f25ead889c18f7c4ebcf785cf69a55fff20a9
Ingeman366 <oliv366x@stud.kea.dk>
2023-11-23 21:44:47 +0100

    small change

--- src/components/monthcalendar/CalendarMonthView.tsx
+++ src/components/monthcalendar/CalendarMonthView.tsx
@@ -11,14 +11,14 @@ const daysinWeek: string[] = ['Mandag', 'Tirdag', 'Onsdag', 'Torsdag', 'Fredag',
 const monthsInYear: string[] = ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'];
 const currentDate = new Date('January, 2027, 02:00:00');
 const CalendarMonthView = () => {
-    
+
     // An object used to set certain values for calculating the calender grid. Updates when the month is changed and renders the page
     const [dateInfo, setDateInfo] = useState({
         monthsInYearIndex: currentDate.getMonth(),
         firstDayOfMonth: startOfMonth(currentDate),
         firstWeekOfMonth: getISOWeek(startOfMonth(currentDate)),
         lastWeekOfMonth: getISOWeek(endOfMonth(startOfMonth(currentDate))),
-      });
+    });
 
     /** 
     * Function that handles the click on the arrows to change the month
@@ -28,7 +28,7 @@ const CalendarMonthView = () => {
         setDateInfo(prevDateInfo => {
             const newDate = new Date(prevDateInfo.firstDayOfMonth.getTime());
             newDate.setMonth(newDate.getMonth() + monthSwapValue);
-    
+
             return {
                 monthsInYearIndex: newDate.getMonth(),
                 firstDayOfMonth: newDate,
@@ -58,72 +58,30 @@ const CalendarMonthView = () => {
     const generateCalenderGrid = () => {
         const rows = [];
         const startDateOfWeek = startOfWeek(dateInfo.firstDayOfMonth, { weekStartsOn: 1 });
-        startDateOfWeek.setHours(startDateOfWeek.getHours() + 1);       
+        startDateOfWeek.setHours(startDateOfWeek.getHours() + 1);
         const lastDayOfMonth: Date = new Date(dateInfo.firstDayOfMonth.getFullYear(), dateInfo.firstDayOfMonth.getMonth() + 1, 0, 23, 59, 59, 999);
-    
-        let endWeek: number = getISOWeek(lastDayOfMonth);
         const numbersOfWeeksInMonth = getWeeksInMonth(startDateOfWeek);
         let firstDayOfWeekToMap: Date = startDateOfWeek;
 
         rows.push(generateWeekDays());
 
-        /*
 
-        // January where first week is 52;
-        if (dateInfo.firstWeekOfMonth === 52) {
-            for (let i = 0; i <= dateInfo.lastWeekOfMonth; i++) {
-                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
-            }
-        } 
-        // January where first week is 53;
-        else if (dateInfo.firstWeekOfMonth === 53) {
-            for (let i = 0; i <= dateInfo.lastWeekOfMonth; i++) {
-                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
-            }
-        }
-        // December where last week is 1;
-        else if (dateInfo.lastWeekOfMonth === 1) {
-            for (let i = dateInfo.firstWeekOfMonth; i <= (dateInfo.firstWeekOfMonth + numbersOfWeeksInMonth); i++) {
-                /*
-                This was needed earlier. Not confident deleting it yet.
-                if (i == 54) {
-                    break;
-                }
-                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
-            }
-        }
-        // December where last week is 53;
-        else if (dateInfo.lastWeekOfMonth === 53) {
-            for (let i = dateInfo.firstWeekOfMonth; i <= endWeek; i++) {
-                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
-            }
+        let startWeek = dateInfo.firstWeekOfMonth;
+        let endWeek: number = getISOWeek(lastDayOfMonth);
+
+        if (dateInfo.lastWeekOfMonth === 1) { //Virker med December som slutter med uge 1
+            endWeek = dateInfo.firstWeekOfMonth + numbersOfWeeksInMonth;
+        } else if (dateInfo.firstWeekOfMonth === 53 || dateInfo.firstWeekOfMonth === 52) { //Virker med Januar som starter med uge 52 og 53
+            startWeek = 0;
         }
 
-        // Regular month
-        else {
-            for (let i = dateInfo.firstWeekOfMonth; i <= dateInfo.lastWeekOfMonth; i++) {
-                rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap))
+        for (let i = startWeek; i <= endWeek; i++) {
+            if (i === 54) {
+                break;
             }
+            rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap));
         }
-        */
-    let startWeek = dateInfo.firstWeekOfMonth;
-    
 
-    if (dateInfo.lastWeekOfMonth === 1) { //Virker med December som slutter med uge 1
-       endWeek = dateInfo.firstWeekOfMonth + numbersOfWeeksInMonth;
-    } else if (dateInfo.firstWeekOfMonth === 53 || dateInfo.firstWeekOfMonth === 52) { //Virker med Januar som starter med uge 52 og 53
-        startWeek = 0;
-    }
-
-    for (let i = startWeek; i <= endWeek; i++) {
-        if (i === 54) {
-            break;
-        }
-        rows.push(generateDatesOfMonth(i, firstDayOfWeekToMap));
-    }
-
-    console.log("Start week: " + startWeek);
-    console.log("End week: " + endWeek);
         return rows;
     }
 
