4ade144745ac17bf318cd2361fdc4497622ff1a4
Alex793x <Alex793x@stud.kea.dk>
2023-11-29 13:22:22 +0100

    frontend async function with token works

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import Link from "next/link";
 import LoginButton from './LoginButton';
+import { useAuth0, GetTokenSilentlyOptions } from '@auth0/auth0-react';
 
 const Login = () => {
+
+  const { getIdTokenClaims, getAccessTokenSilently } = useAuth0();
+
+  const accessToken = getAccessTokenSilently();
+  const idToken = getIdTokenClaims();
+
+  
+  console.log('getIdTokenClaims', accessToken);
+  console.log('getAccessTokenSilently', idToken);
+
   return (
     <>
       <div className=' border-2 border-black p-4 mb-10 hover:cursor-pointer'>
