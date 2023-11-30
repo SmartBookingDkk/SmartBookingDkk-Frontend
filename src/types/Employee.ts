7b3713ad183a0bbd4fc8ca08674acfea157743f5
realkoder <alex8970@stud.kea.dk>
2023-11-30 14:22:28 +0100

    refactored mocked bookings and employee type to match backend entity

--- src/types/Employee.ts
+++ src/types/Employee.ts
@@ -2,15 +2,17 @@ import { User } from './User';
 import { Business } from './Business';
 import { Customer } from './Customer';
 import { Booking } from './Booking';
+import { Address } from './Address';
 
 export type Employee = {
     id: number;
     firstName: string;
     lastName: string;
-    phon: string;
+    phone: string;
     user: User;
     business: Business | null;
     customers: Customer[] | null;
+    address: Address;
     bookings: Booking[] | null;
     createdDate: Date;
     lastModifiedDate: Date;
