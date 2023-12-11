3914d473e7dddcf77466fe5a79607d9b77f71503
realkoder <alex8970@stud.kea.dk>
2023-12-11 13:58:09 +0100

    View profile should be working for customer and employee

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -1,18 +1,17 @@
 'use client'
 
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
 
 // Contexts
 import { ResponsiveProvider } from '@/contexts/MediaQueryContext'
 
 
-
-export function Providers({children}: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-        <ResponsiveProvider>
-                {children}
-        </ResponsiveProvider>
+      <ResponsiveProvider>
+        {children}
+      </ResponsiveProvider>
     </NextUIProvider>
   )
 }
